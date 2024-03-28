1e1acecdeb9d19e2996ac614ce44e36712b98734
Khaled-Dridi <mohamed.khaled.dridi177@gmail.com>
2024-03-29 00:55:02 +0100

    wa

--- app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
@@ -6,20 +6,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import com.example.a7weyjiandroidstudio.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
+    private var selectedOptionText: String = "null"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       // binding.myButton.setOnClickListener{
-       //     binding.myTextView.text="button clicked "
-       // }
+        binding.myButton.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                binding.myTextView.text = "Button clicked"
+                delay(1000) // Delay for 1 second (1000 milliseconds)
+                binding.myTextView.text = selectedOptionText
+            }
+        }
 
+        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.myTextView.text = "Checkbox Checked"
+            } else {
+                binding.myTextView.text = "Checkbox Unchecked"
+            }
 
+        }
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            selectedOptionText = when (checkedId) {
+                R.id.radioButton -> "Option 1 selected"
+                R.id.radioButton2 -> "Option 2 selected"
+                else -> "No option selected"
+            }
+            binding.myTextView.text = "option selected check with the button "
+
+        }
 
     }
 }
