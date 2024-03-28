3a3d5a5da8672e48912a91ca1999d3c1dbbbc561
Khaled-Dridi <mohamed.khaled.dridi177@gmail.com>
2024-03-28 17:40:57 +0100

    a

--- app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7weyjiandroidstudio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,34 +9,17 @@ import com.example.a7weyjiandroidstudio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private var isMainLayout = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+       // binding.myButton.setOnClickListener{
+       //     binding.myTextView.text="button clicked "
+       // }
 
-        binding.button.setOnClickListener {
-            if (isMainLayout) {
-                // Switch to test.xml
-                val testLayout = LayoutInflater.from(this).inflate(R.layout.test, null)
-                setContentView(testLayout)
-                isMainLayout = false
 
-                // Handle click for button2 in test.xml
-                val button2 = testLayout.findViewById<Button>(R.id.button2)
-
-                button2.setOnClickListener {
-                    setContentView(binding.root)
-                    isMainLayout = true
-                }
-            } else {
-                // Switch back to activity_main.xml
-                setContentView(binding.root)
-                isMainLayout = true
-            }
-        }
 
     }
 }
