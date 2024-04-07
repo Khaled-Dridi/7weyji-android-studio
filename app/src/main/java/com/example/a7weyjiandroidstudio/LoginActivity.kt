e6ebfa02cb07c753d97e924ddef59a4a1994cb1c
Khaled-Dridi <mohamed.khaled.dridi177@gmail.com>
2024-04-07 01:18:15 +0100

    eaz

--- app/src/main/java/com/example/a7weyjiandroidstudio/LoginActivity.kt
+++ app/src/main/java/com/example/a7weyjiandroidstudio/LoginActivity.kt
@@ -7,10 +7,9 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a7weyjiandroidstudio.databinding.ActivityLoginBinding
-import com.example.a7weyjiandroidstudio.databinding.ActivityMainBinding
 
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,18 +17,21 @@ class LoginActivity: AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonLogin.setOnClickListener{
-            val username=binding.editTextUsername.text.toString().trim()
-            val password=binding.editTextPassword.text.toString().trim()
-            if(username.isEmpty()||password.isEmpty()){
-                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
-            }else{
+        binding.buttonLogin.setOnClickListener {
+            val username = binding.editTextUsername.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
                 val message = "Username: $username\nPassword: $password"
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
                 // For demonstration, navigate to HomeActivity on successful login
-//                val intent = Intent(this, HomeActivity::class.java)
-//                startActivity(intent)
-//                finish() // Close this LoginActivity
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("USERNAME", username)
+                intent.putExtra("PASSWORD", password)
+                startActivity(intent)
+                finish() // Close this LoginActivity
             }
         }
 
--- app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/a7weyjiandroidstudio/MainActivity.kt
@@ -1,49 +1,51 @@
 package com.example.a7weyjiandroidstudio
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.a7weyjiandroidstudio.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var selectedOptionText: String = "null"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val username = intent.getStringExtra("USERNAME")
+        val password = intent.getStringExtra("PASSWORD")
 
-        binding.myButton.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                binding.myTextView.text = "Button clicked"
-                delay(1000) // Delay for 1 second (1000 milliseconds)
-                binding.myTextView.text = selectedOptionText
-            }
+        binding.myTextView.text = "Username: $username\nPassword: $password"
+
+        binding.imageView1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 3)
         }
 
-        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.myTextView.text = "Checkbox Checked"
-            } else {
-                binding.myTextView.text = "Checkbox Unchecked"
-            }
+    }
 
-        }
-        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-            selectedOptionText = when (checkedId) {
-                R.id.radioButton -> "Option 1 selected"
-                R.id.radioButton2 -> "Option 2 selected"
 
-                else -> "No option selected"
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+            if (selectedImageUri != null) {
+                binding.imageView1.setImageURI(selectedImageUri)
+                // Handle the selected image URI here
+                Log.d("MainActivity", "Selected Image Uri: $selectedImageUri")
+                // You can also load the image into an ImageView
+                // binding.imageView1.setImageURI(selectedImageUri)
             }
-            binding.myTextView.text = "option selected check with the button "
         }
     }
+
 }
